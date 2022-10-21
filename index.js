2b6092c9636e03e1a2b59d289c42dfd85ed14cba
amyhskwon <amyhskwon@gmail.com>
2022-10-21 15:57:39 -0700

    commit tree ed9266144d85a73a5fe1b5a0dee5272c97a16714

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());   
+}
+logShout("hello");
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+logWhisper("hello");
+
+function sayHiToHeadphonedRoommate(string) {
+    console.log(string);
+    console.log(string.toLowerCase());
+    console.log(string.toUpperCase());
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!"
+    }
+    if (string === string.toUpperCase()) {
+        return "YES INDEED!"
+    }
+    if (string === "Let's have dinner together!") {
+        return "I would love to!"
+    }
+}
